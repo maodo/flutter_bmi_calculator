9922f4c9301c6590bfb74f22ff86172ddf31d7dc
maodo <36578673+maodo@users.noreply.github.com>
2022-05-18 19:11:11 +0000

    Widget customization -> customizing reusable card widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const mainColor = 0xFF1D1E33;
+const buttonColor = 0xFFEC1455;
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -17,30 +18,113 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
           leading: Icon(Icons.menu),
         ),
-        body: Column(children: [
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
           Row(children: [
-            Expanded(child: ReusableCard(colour: Color(mainColor),)),
-            Expanded(child: ReusableCard(colour: Color(mainColor),))
+            Expanded(
+                child: ReusableCard(
+              colour: Color(mainColor),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.male,
+                      size: 100.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                          'MALE',
+                          style: TextStyle(
+                              fontSize: 15.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ]),
+            )),
+            Expanded(
+                child: ReusableCard(
+              colour: Color(mainColor),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.female, size: 100.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                          'FEMALE',
+                          style: TextStyle(
+                              fontSize: 15.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )
+                  ]),
+            ))
           ]),
-          Expanded(child: ReusableCard(colour: Color(mainColor),)),
+          Expanded(
+              child: ReusableCard(
+            colour: Color(mainColor),
+            child: Center(
+              child: Text(
+                'Hello',
+              ),
+            ),
+          )),
           Row(children: [
-            Expanded(child: ReusableCard(colour: Color(mainColor),)),
-            Expanded(child: ReusableCard(colour: Color(mainColor),))
-          ])
+            Expanded(
+                child: ReusableCard(
+              colour: Color(mainColor),
+              child: Center(
+                child: Text(
+                  'Hello',
+                ),
+              ),
+            )),
+            Expanded(
+                child: ReusableCard(
+              colour: Color(mainColor),
+              child: Center(
+                child: Text(
+                  'Hello',
+                ),
+              ),
+            )),
+          ]),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15.0, left: 8.0, right: 8.0),
+            child: TextButton(
+              style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(Size.fromHeight(65.0)),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(buttonColor))),
+              onPressed: () {},
+              child: Text(
+                'Calculate your bmi'.toUpperCase(),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+              ),
+            ),
+          )
         ]));
   }
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour});
+  ReusableCard({required this.colour, required this.child});
   final Color colour;
+  final Widget child;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       height: 220.0,
       decoration: BoxDecoration(
-          color: colour, borderRadius: BorderRadius.circular(10.0)),
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: child,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
-
+const primaryColor =  Color(0xFF0A0E21);
+const backgroundColor = Color(0xFF0B0D21);
 class BMICalculator extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: const Color(0xFF0B0D21),
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: backgroundColor,
       ),
       home: InputPage(),
     );
